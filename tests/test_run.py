0feb4965cfde4d385bd52337901e0cbc1a25e6b6
Enrico Zini <enrico@enricozini.org>
2021-08-18 10:54:09 +0200

    Make a test case for each distro

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,15 +1,25 @@
 from __future__ import annotations
 import unittest
+import sys
 import os
 from moncic.unittest import privs, TEST_CHROOTS
 from moncic.machine import Machine
 
 
-class TestRun(unittest.TestCase):
+class RunTestCase:
     def test_run_command(self):
-        for name in TEST_CHROOTS:
-            with self.subTest(name=name):
-                image = os.path.join("images", name)
-                with privs.root():
-                    with Machine(image) as machine:
-                        machine.run(["/usr/bin/true"])
+        image = os.path.join("images", self.distro_name)
+        with privs.root():
+            with Machine(image) as machine:
+                machine.run(["/usr/bin/true"])
+
+
+# Create an instance of RunTestCase for each distribution in TEST_CHROOTS.
+# The test cases will be named Test$DISTRO. For example:
+#   TestCentos7, TestCentos8, TestFedora32, TestFedora34
+this_module = sys.modules[__name__]
+for distro_name in TEST_CHROOTS:
+    cls_name = "Test" + distro_name.capitalize()
+    test_case = type(cls_name, (RunTestCase, unittest.TestCase), {"distro_name": distro_name})
+    test_case.__module__ = __name__
+    setattr(this_module, cls_name, test_case)
