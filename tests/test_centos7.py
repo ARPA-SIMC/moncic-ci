57790ab8b27082b750de0e025b035bd2592fcf0a
Enrico Zini <enrico@enricozini.org>
2022-02-04 15:36:49 +0100

    Added tests for centos bootstrapping

---
+++ tests/test_centos7.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+import re
+import tempfile
+import unittest
+
+from moncic.distro import Distro
+from moncic.unittest import DistroTestMixin
+
+
+class Centos7(DistroTestMixin, unittest.TestCase):
+    def test_bootstrap(self):
+        distro = Distro.create("centos7")
+
+        with self.mock_run(distro) as log:
+            with tempfile.TemporaryDirectory() as workdir:
+                distro.bootstrap_subvolume(workdir)
+
+        log.assertPopFirst(f'btrfs -q subvolume create {workdir}')
+        log.assertPopFirst(re.compile(
+            rf"/usr/bin/dnf -c \S+\.repo -y '--disablerepo=\*' --enablerepo=chroot-base '--disableplugin=\*'"
+            rf' --installroot={workdir} --releasever=7 install bash vim-minimal yum rootfiles dbus'))
+        log.assertLogEmpty()
+
+    def test_upgrade(self):
+        distro = Distro.create("centos7")
+
+        with self.mock_run(distro) as log:
+            with tempfile.TemporaryDirectory() as workdir:
+                distro.update(workdir)
+
+        log.assertPopFirst("/usr/bin/sed -i '/^tsflags=/d' /etc/yum.conf")
+        log.assertPopFirst("/usr/bin/yum install -y epel-release")
+        log.assertPopFirst("/usr/bin/yum install -y @buildsys-build")
+        log.assertPopFirst("/usr/bin/yum install -y yum-utils")
+        log.assertPopFirst("/usr/bin/yum install -y git")
+        log.assertPopFirst("/usr/bin/yum install -y rpmdevtools")
+        log.assertPopFirst("/usr/bin/yum install -q -y yum-plugin-copr")
+        log.assertPopFirst('/usr/bin/yum copr enable -q -y simc/stable epel-7')
+        log.assertPopFirst('/usr/bin/yum upgrade -q -y')
+        log.assertLogEmpty()
