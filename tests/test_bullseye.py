19205109efb1eec09e424b4c848fb1f921333328
Enrico Zini <enrico@enricozini.org>
2022-08-15 09:55:53 +0200

    Added Debian test. refs: #52

---
+++ tests/test_bullseye.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+import re
+import unittest
+
+from moncic.distro import DistroFamily
+from moncic.unittest import DistroTestMixin
+
+
+class Bullseye(DistroTestMixin, unittest.TestCase):
+    def test_bootstrap(self):
+        distro = DistroFamily.lookup_distro("bullseye")
+
+        with self.mock() as run_log:
+            with self.make_images(distro) as images:
+                images.bootstrap_system("test")
+                with images.system("test") as system:
+                    path = system.path
+
+        run_log.assertPopFirstOptional(f'btrfs -q subvolume create {path}.new')
+        run_log.assertPopFirst(re.compile(
+            rf"(/usr/bin/eatmydata )?debootstrap --include=dbus,systemd"
+            rf" --variant=minbase bullseye {path}.new http://deb.debian.org/debian"))
+        run_log.assertLogEmpty()
+
+    def test_upgrade(self):
+        distro = DistroFamily.lookup_distro("bullseye")
+
+        with self.mock() as run_log:
+            with self.make_system(distro) as system:
+                system.update()
+
+        run_log.assertPopFirst('/usr/bin/apt-get update')
+        run_log.assertPopFirst('/usr/bin/apt-get -y upgrade')
+        run_log.assertPopFirst("cachedir_tag:")
+        run_log.assertLogEmpty()
