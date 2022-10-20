64483cc50bb7e6bbd9a3a6b2d791a8884f8e91f1
Emanuele Di Giacomo <edigiacomo@arpae.it>
2022-10-20 19:44:34 +0200

    Fixed type hints for SetnsCallableRunner constructor

--- moncic/runner.py
+++ moncic/runner.py
@@ -295,7 +295,7 @@ class JSONStreamHandler(logging.Handler):
 class SetnsCallableRunner(Runner):
     def __init__(
             self, run: NspawnContainer, config: RunConfig, func: Callable[[], Optional[int]],
-            args: Tuple[Any] = (), kwargs: Optional[Dict[str, any]] = None):
+            args: Tuple = (), kwargs: Optional[Dict[str, Any]] = None):
         super().__init__(run.system.log, config)
         self.run = run
         self.leader_pid = int(run.properties["Leader"])
