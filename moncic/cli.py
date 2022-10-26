7c24f16eebcb7b457d53089d25a7628bc0620f61
Enrico Zini <enrico@enricozini.org>
2022-10-26 16:16:09 +0200

    Synced moncic/cli with other similar code in other projects

--- moncic/cli.py
+++ moncic/cli.py
@@ -1,79 +1,87 @@
 from __future__ import annotations
-from typing import Optional
+
+import argparse
 import logging
 import sys
+from typing import Any, Optional
 
 try:
     import coloredlogs
+
+    HAS_COLOREDLOGS = True
 except ModuleNotFoundError:
-    coloredlogs = None
+    HAS_COLOREDLOGS = False
 
 
-class Fail(RuntimeError):
+def _get_first_docstring_line(obj: Any) -> Optional[str]:
+    try:
+        return obj.__doc__.split("\n")[1].strip()
+    except (AttributeError, IndexError):
+        return None
+
+
+class Fail(BaseException):
     """
-    Exception raised when the program should exit with an error but without a
-    backtrace
+    Failure that causes the program to exit with an error message.
+
+    No stack trace is printed.
     """
     pass
 
 
 class Command:
-    # Command name (as used in command line)
-    # Defaults to the lowercased class name
+    """
+    Base class for actions run from command line
+    """
+
     NAME: Optional[str] = None
 
-    # Command description (as used in command line help)
-    # Defaults to the strip()ped class docstring.
-    DESC: Optional[str] = None
-
-    def __init__(self, args):
+    def __init__(self, args: argparse.Namespace):
+        if self.NAME is None:
+            self.NAME = self.__class__.__name__.lower()
         self.args = args
         self.setup_logging()
 
-    def setup_logging(self):
-        # Setup logging
-        FORMAT = "%(asctime)-19.19s %(levelname)s %(name)s %(message)s"
+    def setup_logging(self) -> None:
+        FORMAT = "%(asctime)-15s %(levelname)s %(name)s %(message)s"
         if self.args.debug:
-            log_level = logging.DEBUG
+            level = logging.DEBUG
         elif self.args.verbose:
-            log_level = logging.INFO
+            level = logging.INFO
         else:
-            log_level = logging.WARN
+            level = logging.WARN
 
-        if coloredlogs is not None:
-            coloredlogs.install(level=log_level, fmt=FORMAT)
+        if HAS_COLOREDLOGS:
+            coloredlogs.install(level=level, fmt=FORMAT)
         else:
-            logging.basicConfig(level=log_level, stream=sys.stderr, format=FORMAT)
-
-        # log_handler = logging.StreamHandler(sys.stderr)
-        # log_handler.setFormatter(logging.Formatter(FORMAT))
-        # log_handler.setLevel(log_level)
-        # root_logger = logging.getLogger()
-        # root_logger.addHandler(log_handler)
-        # root_logger.setLevel(logging.DEBUG)
-
-    @classmethod
-    def get_name(cls):
-        if cls.NAME is not None:
-            return cls.NAME
-        return cls.__name__.lower()
+            logging.basicConfig(level=level, stream=sys.stderr, format=FORMAT)
 
     @classmethod
     def make_subparser(cls, subparsers):
-        desc = cls.DESC
-        if desc is None:
-            desc = cls.__doc__.strip()
-
-        parser = subparsers.add_parser(cls.get_name(), help=desc)
-        parser.set_defaults(handler=cls)
-        parser.add_argument("-v", "--verbose", action="store_true", help="verbose output")
-        parser.add_argument("--debug", action="store_true", help="verbose output")
+        if cls.NAME is None:
+            cls.NAME = cls.__name__.lower()
+        parser = subparsers.add_parser(
+            cls.NAME,
+            help=_get_first_docstring_line(cls),
+        )
+        parser.set_defaults(command=cls)
+        parser.add_argument(
+            "-v",
+            "--verbose",
+            action="store_true",
+            help="verbose output",
+        ),
+        parser.add_argument(
+            "--debug",
+            action="store_true",
+            help="debugging output",
+        ),
         return parser
 
 
 def run(func):
     try:
-        func()
+        sys.exit(func())
     except Fail as e:
         print(e, file=sys.stderr)
         sys.exit(1)
