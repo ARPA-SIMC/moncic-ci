54dd9de0a81b63e28774c2b61a232025646e4513
Enrico Zini <enrico@enricozini.org>
2023-03-16 13:20:53 +0100

    Fixed tests after introducing more build options

--- tests/test_build.py
+++ tests/test_build.py
@@ -6,24 +6,28 @@ from moncic.build.build import Build
 from moncic.build.debian import Debian
 from moncic.build.arpa import RPM, ARPA
 
+COMMON_BUILD_PROFILES = [
+    "artifacts_dir", "source_only", "on_success", "on_fail", "on_end"
+]
+
 
 class TestBuild(unittest.TestCase):
     def test_build(self):
         self.assertEqual(
             [x[0] for x in Build.list_build_options()],
-            ["artifacts_dir", "source_only"])
+            COMMON_BUILD_PROFILES)
 
     def test_debian(self):
         self.assertEqual(
             [x[0] for x in Debian.list_build_options()],
-            ["artifacts_dir", "source_only", "build_profile"])
+            COMMON_BUILD_PROFILES + ["build_profile"])
 
     def test_rpm(self):
         self.assertEqual(
             [x[0] for x in RPM.list_build_options()],
-            ["artifacts_dir", "source_only"])
+            COMMON_BUILD_PROFILES)
 
     def test_arpa(self):
         self.assertEqual(
             [x[0] for x in ARPA.list_build_options()],
-            ["artifacts_dir", "source_only"])
+            COMMON_BUILD_PROFILES)
