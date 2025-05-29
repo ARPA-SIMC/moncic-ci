2f7963d072864231745b7b287f246dc793d77f0d
Enrico Zini <enrico@enricozini.org>
2025-05-29 17:48:54 +0200

    Manage a persistent integration tests environment

---
+++ itests/base.py
@@ -0,0 +1,121 @@
+import abc
+import contextlib
+import logging
+import os
+import sys
+from collections.abc import Generator
+from pathlib import Path
+from typing import ClassVar, override
+
+from rich.logging import RichHandler
+
+from moncic.distro import Distro, DistroFamily
+from moncic.image import RunnableImage
+from moncic.images import BootstrappingImages
+from moncic.moncic import Moncic, MoncicConfig
+from moncic.nspawn.images import BtrfsImages, NspawnImages, PlainImages
+from moncic.podman.images import PodmanImages
+from moncic.provision.images import DistroImages
+from moncic.session import Session
+from moncic.unittest import MoncicTestCase
+from moncic.utils.btrfs import is_btrfs
+
+
+class IntegrationTestsBase(MoncicTestCase, abc.ABC):
+    distro: ClassVar[Distro]
+    session: ClassVar[Session]
+    distro_images: ClassVar[DistroImages]
+    images: ClassVar[BootstrappingImages]
+    bootstrapped: ClassVar[RunnableImage | None]
+
+    @classmethod
+    def get_config(self, name: str) -> str:
+        """Return an integration test configuration name passed by the ./test script."""
+        if (value := os.environ.get(f"MONCIC_ITESTS_{name.upper()}", None)) is None:
+            raise RuntimeError("integration tests need to be run using `./test -i`")
+        return value
+
+    @override
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.session = cls.make_session()
+        cls.distro_images = DistroImages(cls.session)
+        cls.images = cls.make_images()
+        cls.bootstrapped = None
+
+    @classmethod
+    def make_session(cls) -> Session:
+        config = MoncicConfig()
+        config.imageconfdirs = []
+        config.auto_sudo = False
+        config.deb_cache_dir = None
+        return cls.enterClassContext(Moncic(config).session())
+
+    @classmethod
+    @abc.abstractmethod
+    def make_images(cls) -> BootstrappingImages: ...
+
+    @classmethod
+    def get_bootstrapped(cls) -> RunnableImage:
+        if not cls.bootstrapped:
+            with cls.verbose_logging():
+                bimage = cls.distro_images.image(cls.distro.name)
+                cls.bootstrapped = cls.images.bootstrap(bimage)
+        return cls.bootstrapped
+
+    @classmethod
+    @contextlib.contextmanager
+    def verbose_logging(cls) -> Generator[None, None, None]:
+        print()
+        handler = RichHandler()
+        handler.setLevel(logging.INFO)
+        root_logger = logging.getLogger()
+        orig_root_level = root_logger.level
+        root_logger.setLevel(logging.INFO)
+        root_logger.addHandler(handler)
+        try:
+            yield
+        finally:
+            root_logger.setLevel(orig_root_level)
+            root_logger.removeHandler(handler)
+
+
+class NspawnIntegrationTestsBase(IntegrationTestsBase, abc.ABC):
+    @override
+    @classmethod
+    def make_images(cls) -> NspawnImages:
+        image_path = Path(cls.get_config("workdir"))
+        image_path.mkdir(exist_ok=True, parents=True)
+        if is_btrfs(image_path):
+            return BtrfsImages(cls.session, image_path)
+        else:
+            return PlainImages(cls.session, image_path)
+
+
+class PodmanIntegrationTestsBase(IntegrationTestsBase, abc.ABC):
+    @override
+    @classmethod
+    def make_session(cls) -> Session:
+        session = super().make_session()
+        session.podman_repository = cls.get_config("podman_repo")
+        return session
+
+    @override
+    @classmethod
+    def make_images(cls) -> PodmanImages:
+        return PodmanImages(cls.session)
+
+
+def setup_distro_tests(module_name: str, bases: dict[str, type[IntegrationTestsBase]], suffix: str) -> None:
+    this_module = sys.modules[module_name]
+    for distro_family in DistroFamily.list_families():
+        for distro_name in sorted({di.name for di in distro_family.list_distros()}):
+            distro = DistroFamily.lookup_distro(distro_name)
+            for tech in "nspawn", "podman":
+                base = bases[tech]
+                name = "".join(n.capitalize() for n in distro.name.split(":"))
+                cls_name = name + tech.capitalize() + suffix
+                test_case = type(cls_name, (base,), {"distro": distro})
+                test_case.__module__ = __name__
+                setattr(this_module, cls_name, test_case)
--- itests/test_maintenance.py
+++ itests/test_maintenance.py
@@ -1,80 +1,9 @@
 import abc
-import contextlib
-import logging
-import shutil
-import sys
-import tempfile
-from collections.abc import Generator
-from pathlib import Path
-from typing import ClassVar, override
 
-from rich.logging import RichHandler
-
-from moncic import context
-from moncic.distro import Distro, DistroFamily
-from moncic.image import RunnableImage
-from moncic.images import BootstrappingImages
-from moncic.moncic import Moncic, MoncicConfig
-from moncic.nspawn.images import BtrfsImages, NspawnImages, PlainImages
-from moncic.podman.images import PodmanImages
-from moncic.provision.images import DistroImages
-from moncic.session import Session
-from moncic.unittest import MoncicTestCase
-from moncic.utils.btrfs import is_btrfs
+from .base import IntegrationTestsBase, NspawnIntegrationTestsBase, PodmanIntegrationTestsBase, setup_distro_tests
 
 
-class DistroMaintenanceTests(MoncicTestCase, abc.ABC):
-    distro: ClassVar[Distro]
-    session: ClassVar[Session]
-    distro_images: ClassVar[DistroImages]
-    images: ClassVar[BootstrappingImages]
-    bootstrapped: ClassVar[RunnableImage | None]
-
-    @override
-    @classmethod
-    def setUpClass(cls) -> None:
-        super().setUpClass()
-        cls.session = cls.make_session()
-        cls.distro_images = DistroImages(cls.session)
-        cls.images = cls.make_images()
-        cls.bootstrapped = None
-
-    @classmethod
-    def make_session(cls) -> Session:
-        config = MoncicConfig()
-        config.imageconfdirs = []
-        config.auto_sudo = False
-        config.deb_cache_dir = None
-        return cls.enterClassContext(Moncic(config).session())
-
-    @classmethod
-    @abc.abstractmethod
-    def make_images(cls) -> BootstrappingImages: ...
-
-    @classmethod
-    def get_bootstrapped(cls) -> RunnableImage:
-        if not cls.bootstrapped:
-            with cls.verbose_logging():
-                bimage = cls.distro_images.image(cls.distro.name)
-                cls.bootstrapped = cls.images.bootstrap(bimage)
-        return cls.bootstrapped
-
-    @classmethod
-    @contextlib.contextmanager
-    def verbose_logging(cls) -> Generator[None, None, None]:
-        print()
-        handler = RichHandler()
-        handler.setLevel(logging.INFO)
-        root_logger = logging.getLogger()
-        orig_root_level = root_logger.level
-        root_logger.setLevel(logging.INFO)
-        root_logger.addHandler(handler)
-        try:
-            yield
-        finally:
-            root_logger.setLevel(orig_root_level)
-            root_logger.removeHandler(handler)
-
+class DistroMaintenanceTests(IntegrationTestsBase, abc.ABC):
     def test_bootstrap(self) -> None:
         self.get_bootstrapped()
 
@@ -82,45 +11,24 @@ class DistroMaintenanceTests(MoncicTestCase, abc.ABC):
         rimage = self.get_bootstrapped()
         rimage.update()
 
+    def test_run(self) -> None:
+        # TODO: run cat /etc/os-release, use it to lookup a distro and verify it's the same as self.distro
+        pass
+
     # def test_remove(self) -> None:
+    #     TODO: make a pretend image for nspawn
     #     raise NotImplementedError()
 
+    # On another integration test, making images persist across TestCases
     # def test_build(self) -> None:
     #     raise NotImplementedError()
 
 
-class NspawnDistroMaintenanceTests(DistroMaintenanceTests, abc.ABC):
-    @override
-    @classmethod
-    def tearDownClass(cls) -> None:
-        with context.privs.root():
-            assert isinstance(cls.images, NspawnImages)
-            shutil.rmtree(cls.images.imagedir)
-        super().tearDownClass()
-
-    @override
-    @classmethod
-    def make_images(cls) -> NspawnImages:
-        image_path = Path(cls.enterClassContext(tempfile.TemporaryDirectory(delete=False)))
-        if is_btrfs(image_path):
-            return BtrfsImages(cls.session, image_path)
-        else:
-            return PlainImages(cls.session, image_path)
+class NspawnDistroMaintenanceTests(DistroMaintenanceTests, NspawnIntegrationTestsBase, abc.ABC):
+    pass
 
 
-class PodmanDistroMaintenanceTests(DistroMaintenanceTests, abc.ABC):
-    @override
-    @classmethod
-    def make_session(cls) -> Session:
-        session = super().make_session()
-        session.podman_repository = "localhost/moncic-ci-tests"
-        return session
-
-    @override
-    @classmethod
-    def make_images(cls) -> PodmanImages:
-        return PodmanImages(cls.session)
-
+class PodmanDistroMaintenanceTests(DistroMaintenanceTests, PodmanIntegrationTestsBase, abc.ABC):
     def test_get_podman_name(self) -> None:
         repo, tag = self.distro.get_podman_name()
         name = f"{repo}:{tag}"
@@ -129,31 +37,13 @@ class PodmanDistroMaintenanceTests(DistroMaintenanceTests, abc.ABC):
             self.assertTrue(self.session.podman.images.exists(name))
 
 
-bases: dict[str, type[DistroMaintenanceTests]] = {
+bases: dict[str, type[IntegrationTestsBase]] = {
     "nspawn": NspawnDistroMaintenanceTests,
     "podman": PodmanDistroMaintenanceTests,
 }
 
 
-def setup_tests() -> None:
-
-    # Create an instance of DistroMaintenanceTests for each distribution in TEST_CHROOTS.
-    # The test cases will be named Test$DISTRO. For example:
-    #   TestCentos7, TestCentos8, TestFedora32, TestFedora34
-    this_module = sys.modules[__name__]
-    for distro_family in DistroFamily.list_families():
-        for distro_name in sorted({di.name for di in distro_family.list_distros()}):
-            distro = DistroFamily.lookup_distro(distro_name)
-            for tech in "nspawn", "podman":
-                base = bases[tech]
-                name = "".join(n.capitalize() for n in distro.name.split(":"))
-                cls_name = name + tech.capitalize() + "DistroMaintenanceTests"
-                test_case = type(cls_name, (base,), {"distro": distro})
-                test_case.__module__ = __name__
-                setattr(this_module, cls_name, test_case)
-
-
-setup_tests()
+setup_distro_tests(__name__, bases, "DistroMaintenanceTests")
 
 del NspawnDistroMaintenanceTests
 del PodmanDistroMaintenanceTests
